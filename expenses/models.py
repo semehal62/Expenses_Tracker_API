29a0e5b93144705774c7e3f3b59aa2e071b81b69
semehal62 <semehalhailu4@gmail.com>
2025-12-31 01:16:18 +0300

    fix(expenses): link Expense to custom user model

--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,8 @@
-from django.db import models
 from django.conf import settings
-
+from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -11,16 +10,18 @@ class Category(models.Model):
 
 class Expense(models.Model):
     user = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE,
-    related_name='expenses'
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="expenses"
     )
-
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="expenses"
+    )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
-    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.amount}"
-
+        return f"{self.user} - {self.amount}"
