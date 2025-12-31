c0c07637986049b5f046f7506b0908b699bc43cb
semehal62 <semehalhailu4@gmail.com>
2025-12-31 12:47:04 +0300

    fixing the urls of expens and config

--- expenses/models.py
+++ expenses/models.py
@@ -19,6 +19,7 @@ class Expense(models.Model):
         on_delete=models.CASCADE,
         related_name="expenses"
     )
+    title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Expense
+from .serializers import ExpenseSerializer
 
-# Create your views here.
+
+class ExpenseViewSet(ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
